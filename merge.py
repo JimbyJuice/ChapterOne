51b23e0a38700ee903165e0b801f1ccd42e23cb2
Yousha Kabir <youshasafwan@gmail.com>
2026-04-10 01:46:29 +1000

    feat: Initial Python Code for Merge Sort

---
+++ merge.py
@@ -0,0 +1,59 @@
+LEFT = 1
+RIGHT = 2
+GRADE = ['S', 'A', 'B', 'C', 'D' 'F']
+def mergeSort(arr):
+    length = len(arr)
+    if length <= 1:
+        return arr
+    
+    left_arr = arr[:length//2]
+    right_arr = arr[length//2:]
+
+    mergeSort(left_arr)
+    mergeSort(right_arr)
+    
+    # merge
+    i = 0
+    j = 0
+    k = 0
+    while i < len(left_arr) and j < len(right_arr):
+        while True:
+            try:
+                choice = int(input(f"{left_arr[i]} or {right_arr[j]}: "))
+                
+                if choice != LEFT and choice != RIGHT:
+                    raise ValueError("Not a valid option")
+            
+            except ValueError:
+                print("Please pick either 1 or 2")
+                continue
+            
+            break
+                
+        if choice == LEFT:
+            arr[k] = left_arr[i]
+            i += 1
+        elif choice == RIGHT:
+            arr[k] = right_arr[j]
+            j += 1
+            
+        k += 1
+    
+    while i < len(left_arr):
+        arr[k] = left_arr[i]
+        i += 1
+        k += 1
+        
+    while j < len(right_arr):
+        arr[k] = right_arr[j]
+        j += 1
+        k += 1
+    
+    return arr
+    
+nums = ['Yallah Eats', "Lanzhou", "TFT", "Krazy Bird", "Burger Bros", "Sambal"]
+
+print(mergeSort(nums))
+    
+
+
